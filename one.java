f1b3b01e2c02fde2ef3eb1334ea7520c231a909e
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-05 00:09:07 +0530

    this is loop program

--- one.java
+++ one.java
@@ -1,3 +1,13 @@
 public class one {
-    
+    public static void main(String args[]){
+        System.out.println("this is loops");
+        for(int i=0;  i<5;  i++){
+            System.out.println("this is loop");
+        }
+        System.out.println("this is end of first loop");
+        for(int i=0; i<4; i++){
+            System.out.println("this  is second loop");
+        }
+        System.out.println("this is end of second loop");
+    }
 }
