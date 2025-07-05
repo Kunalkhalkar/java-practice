a722fc86e0d77edfd2e8a09d3b7f3e09c2a2b663
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-05 23:50:58 +0530

    this is while loop program

---
+++ three.java
@@ -0,0 +1,23 @@
+public class three{
+	public static void main (String [] args){
+		System.out.println("day three of java full stack ");
+		System.out.println("while loop");
+		int i=0;
+		while(i<5){
+			System.out.println("i = "+ i);
+			i++;	
+			System.out.println("i incremented to " + i);
+			}
+			System.out.println("while loop ended");
+			// This is a simple Java program demonstrating a while loop 1
+			int j=0;
+
+			while(j<5){
+				System.out.println("j = "+ j );
+				++j;
+				System.out.println("j incremented to " + j);
+			}
+	}
+	
+// This is a simple Java program demonstrating a while loop 2
+}
