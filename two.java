a9ea2a717067c5f86f95c3beec62d4373b3363e1
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-06 00:02:33 +0530

    for loop added

--- two.java
+++ two.java
@@ -1,5 +1,8 @@
 public class two {
     public static void main(String [] args){
+	for(int j=0; j<5; j++){
+		System.out.println("this is for loop used to print one line, multiple times");
+	}
 
     }
 }
