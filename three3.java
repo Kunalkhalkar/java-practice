419c2e04bc1c65bb0ed221b7549e3aa2515a312e
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-06 00:23:39 +0530

    this is third while loop program

---
+++ three3.java
@@ -0,0 +1,22 @@
+public class three3{
+	public static void main(String [] args){
+		System.out.println("this is another example of while loop");
+		int i=0;
+
+		while(i>0){
+			System.out.println("this loop has wrong parameters");
+			i++;
+}
+		System.out.println("this loop will not execute because i is not less than 0");
+		System.out.println("this is the end of the program");
+		
+		int j=0;
+		while(j<5){
+			System.out.println("j = "+ j);
+			j++;
+			System.out.println("j incremented to " + j);
+		}
+		System.out.println("while loop ended");
+		// This is a simple Java program demonstrating a while loop
+}
+}
