0cf15c8bf99df3629ed454cbec419340c5943c7f
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-06 00:13:59 +0530

    this is second while loop program

---
+++ three2.java
@@ -0,0 +1,12 @@
+public class three2{
+	public static void main(String [] args){
+		int w=0;
+		while(w<10){
+			System.out.println("value of w is = "+ w);
+		++w;
+			System.out.println("value of w is = "+ w);
+}
+
+}
+
+}
