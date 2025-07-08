aab6c6620c5fe9c8ab4a4a77a38b30f06fcdf14b
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-08 23:34:00 +0530

    new example using while loop

---
+++ trial2.java
@@ -0,0 +1,12 @@
+public class trial2{
+	public static void main(String x[]){
+	System.out.println("this program is uses loop to print star (*)");
+
+	for(int s= 0; s<=4; s++){
+		for(int j=0; j<s; j++){
+		System.out.print("*");
+}
+		System.out.println();
+		}
+}
+}
