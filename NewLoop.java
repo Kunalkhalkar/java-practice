262169ac04ffacb795fc0920fb8d6b69d20ed258
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-08 22:10:26 +0530

    this is DO-While loop program

---
+++ NewLoop.java
@@ -0,0 +1,17 @@
+public class NewLoop{
+
+		public static void main(String X[]){
+			System.out.println("this is DO-While loop");
+			System.out.println("-----------------------------------------");
+
+		int m = 0;
+		do{
+		
+		System.out.println("this the "+ m +" term");
+		m++;
+	
+			}
+	
+		while(m<10);			}
+ 
+}
