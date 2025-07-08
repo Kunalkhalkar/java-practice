64accfa48d5ea3971ab8a2037262d72e8295e2e8
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-08 22:43:48 +0530

    new example using loop

---
+++ AZ.java
@@ -0,0 +1,13 @@
+public class AZ{
+		public static void main(String x[]){
+			System.out.println("this is the loop which is used to print A to Z charactors");
+			
+			char k='A';
+			do{
+				System.out.println("character = " + k);
+				k++;	
+			  } while( k<='Z');
+	
+	}	
+		
+	}
---
+++ trial.java
@@ -0,0 +1,9 @@
+public class trial{
+	public static void main(String x[]){
+		System.out.println("this program is used to calculate addition without using + operator");
+		int a =-10, b=-20;
+		int c = -(-a-b);
+		System.out.println("answer is "+ c);
+	}
+
+}
