1252d195e9e3c0540c7878afe7232e098af51534
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-08 23:02:26 +0530

    new example using while loop

---
+++ trial1.java
@@ -0,0 +1,18 @@
+public class trial1{
+	public static void main(String x[]){
+	
+	System.out.println("this program used to print a to z using While Loop");
+		
+	char k = 'a';
+	int c = 1; 
+	while( k<='z'){
+			
+		System.out.println("alphabet " +c+ " = " +k);
+		k++;
+		c++;
+	}
+
+
+
+}
+}
