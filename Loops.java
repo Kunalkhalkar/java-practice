0d67632491ca729483ff847e19c598c16dce280e
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-08 20:34:07 +0530

    this is for loop added...

---
+++ Loops.java
@@ -0,0 +1,18 @@
+public class Loops{
+	public static void main(String args[]){
+		System.out.println("this is  while Loop\n");
+		int x = 1;
+		while(x < 10){
+			System.out.println ("this is while loop executed " + x + " Times");
+			x++;
+				}	
+		for(int m = 10; m>=1; m--){
+		System.out.println("this is for loop executing " + m + " times");
+
+						}
+		
+
+			}
+
+
+}
