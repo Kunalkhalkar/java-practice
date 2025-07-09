e19c4743a539ce8b23a11fb4b9e4a7a86b61f240
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-07-09 23:18:41 +0530

    input using command line argument"

---
+++ operator.java
@@ -0,0 +1,15 @@
+public class operator{
+
+	public static void main(String x['0', '1']){
+		System.out.println("--------------------------------------------------------");
+		System.out.println("Accepting the values from command line argument");
+		System.out.println("--------------------------------------------------------");
+		int a=Integer.parseInt(x[0]);
+		int b=Integer.parseInt(x[1]);
+		int c=Integer.parseInt(x[0])+Integer.parseInt(x[1]);
+
+		System.out.println("values of a = "+a +" and b is = "+ b +" Addition is = "+ c);
+	}
+
+	
+}
