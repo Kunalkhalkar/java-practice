4631ff6ccb1f478729c871e87682fcd845e8a351
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-05-11 20:44:26 +0530

    Fix loop structure to print characters A to Z

--- AZ.java
+++ AZ.java
@@ -2,10 +2,12 @@ public class AZ{
 		public static void main(String x[]){
 			System.out.println("this is the loop which is used to print A to Z charactors");
 			char k='A';
+			
 			do{
 				System.out.println("character = " + k);
 				k++;	
-			  } while( k<='Z');
+			  } 
+				while( k<='Z');
 	
 	}	
 		
