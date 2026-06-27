6c222fa056c242f0906b8f5760df415f3ea7f5bc
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-06-27 22:52:42 +0530

    Update AZ.java

--- AZ.java
+++ AZ.java
@@ -2,7 +2,6 @@ public class AZ{
 		public static void main(String x[]){
 			System.out.println("this is the loop which is used to print A to Z charactors");
 			char k='A';
-			
 			do{
 				System.out.println("character = " + k);
 				k++;	
